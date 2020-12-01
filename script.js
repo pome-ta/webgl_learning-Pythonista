bb9ac7857fa1c4e21536d10af46c53ed22a32d1d
pome-ta <pome-ta@users.noreply.github.com>
2020-12-01 22:43:59 +0900

    comment out

--- script.js
+++ script.js
@@ -17,10 +17,9 @@ onload = () => {
   // webglコンテキストを取得
   const gl = c.getContext('webgl') || c.getContext('experimental-webgl');
 
-  // canvasを初期化する色を設定する
+  // canvasを初期化する色を設定
+  // canvasを初期化する際の深度を設定
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // canvasを初期化する際の深度を設定する
   gl.clearDepth(1.0);
 
   // canvasを初期化
